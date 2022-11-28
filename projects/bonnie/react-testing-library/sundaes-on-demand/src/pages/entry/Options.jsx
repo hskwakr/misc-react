b1da77718e7d93db1d32ee133dd8fcdcab332ba7
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-11-28 12:53:56 +0900

    Update example
    52. OPTIONAL React Code: Options and ScoopOption Components

---
+++ projects/bonnie/react-testing-library/sundaes-on-demand/src/pages/entry/Options.jsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Row } from "react-bootstrap";
+import ScoopOption from "./ScoopOption";
+
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {});
+  }, [optionType]);
+
+  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
+}
