118912eb681e35801c06261e24d01437c0d0fc2e
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-11-28 13:22:00 +0900

    Update example
    54. Code Quiz! Topping Options from Server

--- projects/bonnie/react-testing-library/sundaes-on-demand/src/pages/entry/Options.jsx
+++ projects/bonnie/react-testing-library/sundaes-on-demand/src/pages/entry/Options.jsx
@@ -2,23 +2,26 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Row } from "react-bootstrap";
 import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-    async function fetch() {
+    async function fetch(req) {
       try {
-        const res = await axios.get(`http://localhost:3030/${optionType}`);
+        const res = await axios.get(req);
         setItems(res.data);
       } catch (error) {
         // todo
       }
     }
-    fetch();
+
+    fetch(`http://localhost:3030/${optionType}`);
   }, [optionType]);
 
-  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+  const ItemComponent =
+    optionType === "scoops" ? ScoopOption : "toppings" ? ToppingOption : null;
 
   const optionItems = items.map((item) => (
     <ItemComponent
