b259a8542b939d9aceb408c1eff1aa75f5f4094f
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-11-29 08:26:40 +0900

    Update OrderEntry.test.jsx
    59. Running only Selected Tests, and `waitFor`

--- projects/bonnie/react-testing-library/sundaes-on-demand/src/pages/entry/test/OrderEntry.test.jsx
+++ projects/bonnie/react-testing-library/sundaes-on-demand/src/pages/entry/test/OrderEntry.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { rest } from "msw";
 import { server } from "../../../mocks/server";
 
@@ -16,9 +16,8 @@ test("handles error for scoops and toppings routes", async () => {
 
   render(<OrderEntry />);
 
-  const alerts = await screen.findAllByRole("alert", {
-    name: /An unexpected error ocurred. Please try again later./i,
+  await waitFor(async () => {
+    const alerts = await screen.findAllByRole("alert");
+    expect(alerts).toHaveLength(2);
   });
-
-  expect(alerts).toHaveLength(2);
 });
