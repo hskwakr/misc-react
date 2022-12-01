7eaf65f9c0c3b214584fd3f00859e0b638c2d984
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-01 10:11:35 +0900

    Update ToppingOption.jsx
    71. OPTIONAL React Code: Toppings Checkboxes

--- projects/bonnie/react-testing-library/sundaes-on-demand/src/pages/entry/ToppingOption.jsx
+++ projects/bonnie/react-testing-library/sundaes-on-demand/src/pages/entry/ToppingOption.jsx
@@ -1,13 +1,23 @@
-import { Col } from "react-bootstrap";
+import { Col, Form } from "react-bootstrap";
+import { useOrderDetails } from "../../contexts/OrderDetails";
 
 export default function ToppingOption({ name, imagePath }) {
+  const { updateItemCount } = useOrderDetails();
+
+  const handleChange = (e) => {
+    updateItemCount(name, e.target.checked ? 1 : 0, "toppings");
+  };
+
   return (
-    <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
+    <Col xs={6} sm={4} md={3} lg={2} style={{ textAlign: "center" }}>
       <img
         style={{ width: "75%" }}
         src={`http://localhost:3030/${imagePath}`}
         alt={`${name} topping`}
       />
+      <Form.Group controlId={`${name}-count`}>
+        <Form.Check type="checkbox" onChange={handleChange} label={name} />
+      </Form.Group>
     </Col>
   );
 }
