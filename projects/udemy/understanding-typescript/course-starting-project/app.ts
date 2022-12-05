05f2c272494260586365b20e2771762297ea7377
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-05 12:29:35 +0900

    Update app.ts
    18. Array Types

--- projects/udemy/understanding-typescript/course-starting-project/app.ts
+++ projects/udemy/understanding-typescript/course-starting-project/app.ts
@@ -1,9 +1,18 @@
-const person: {
-  name: string;
-  age: number;
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
   name: "Maximilian",
   age: 30,
+  hobbies: ["Sports", "Cooking"],
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
