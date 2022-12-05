511e7dc2da5acc6a78e9b648c205a6b038065991
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-05 14:40:31 +0900

    Update app.ts
    21. Working with Enums

--- projects/udemy/understanding-typescript/course-starting-project/app.ts
+++ projects/udemy/understanding-typescript/course-starting-project/app.ts
@@ -2,20 +2,30 @@
 //   name: string;
 //   age: number;
 // } = {
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Maximilian",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: "Maximilian",
   age: 30,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
+  role: Role.READ_ONLY,
 };
-
-person.role.push("admin");
-person.role = [0, "admin"];
 
 let favoriteActivities: string[];
 favoriteActivities = ["Sports"];
@@ -24,4 +34,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.READ_ONLY) {
+  console.log("Read only user");
 }
