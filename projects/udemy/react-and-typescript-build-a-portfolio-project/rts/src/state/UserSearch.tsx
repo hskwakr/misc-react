696fed7c61930d48b60e7963dabc285b1ee59432
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-21 08:41:49 +0900

    Update UserSearch.tsx
    17. Type Unions in State

--- projects/udemy/react-and-typescript-build-a-portfolio-project/rts/src/state/UserSearch.tsx
+++ projects/udemy/react-and-typescript-build-a-portfolio-project/rts/src/state/UserSearch.tsx
@@ -8,15 +8,29 @@ const users = [
 
 const UserSearch = () => {
   const [name, setName] = useState('');
+  const [foundUsers, setFoundUsers] = useState<typeof users>([]);
 
   const onChangeHandler: React.ChangeEventHandler<HTMLInputElement> = (e) => {
     setName(e.target.value);
   };
 
   const onClickHandler = () => {
-    const found = users.filter((u) => u.name.includes(name));
-    console.log(found);
+    const found = users.filter((u) => name !== '' && u.name.includes(name));
+    setFoundUsers(found);
   };
+
+  const searchResult =
+    foundUsers.length > 0 ? (
+      <ul>
+        {foundUsers.map((user) => (
+          <li key={user.name}>
+            {user.name} {user.age}
+          </li>
+        ))}
+      </ul>
+    ) : (
+      <p>User Not Found</p>
+    );
 
   return (
     <div>
@@ -27,6 +41,7 @@ const UserSearch = () => {
           Find User
         </button>
       </div>
+      {searchResult}
     </div>
   );
 };
