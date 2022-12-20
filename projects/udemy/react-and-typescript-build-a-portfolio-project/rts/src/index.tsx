2f284090d2867299a01140a855f6d929052b47b9
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-21 07:17:16 +0900

    Update example of RTBP
    14. Type Inference with State

--- projects/udemy/react-and-typescript-build-a-portfolio-project/rts/src/index.tsx
+++ projects/udemy/react-and-typescript-build-a-portfolio-project/rts/src/index.tsx
@@ -1,14 +1,14 @@
 import ReactDOM from 'react-dom/client';
+import GuestList from './state/GuestList';
 
 const el = document.getElementById('root');
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = ReactDOM.createRoot(el!);
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hi there!</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <GuestList />
+  </div>
+);
 
 root.render(<App />);
---
+++ projects/udemy/react-and-typescript-build-a-portfolio-project/rts/src/state/GuestList.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+const GuestList = () => {
+  const [name, setName] = useState('');
+  const [guests, setGuests] = useState<typeof name[]>([]);
+
+  const onChangeHandler: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setName(e.target.value);
+  };
+
+  const onClickHandler = () => {
+    setName('');
+    setGuests((preGuests) => [...preGuests, name]);
+  };
+
+  return (
+    <div>
+      <h3>Guest List</h3>
+
+      <ul>
+        {guests.map((guest) => (
+          <li>{guest}</li>
+        ))}
+      </ul>
+
+      <input type="text" value={name} onChange={onChangeHandler} />
+      <button type="button" onClick={onClickHandler}>
+        {' '}
+        Add Guest
+      </button>
+    </div>
+  );
+};
+
+export default GuestList;
