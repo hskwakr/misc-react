f1cb990edd3911dd8b5726470829746e5c174418
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-20 13:31:33 +0900

    Update example of RTBP
    8. Changes with TypeScript

--- projects/udemy/react-and-typescript-build-a-portfolio-project/rts/src/index.tsx
+++ projects/udemy/react-and-typescript-build-a-portfolio-project/rts/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el!);
 
-function App() {
+const App = () => {
   return (
     <div>
       <h1>Hi there!</h1>
---
+++ projects/udemy/react-and-typescript-build-a-portfolio-project/rts/src/props/Parent.tsx
@@ -0,0 +1,5 @@
+import { Child } from './Child';
+
+const Parent = () => <Child />;
+
+export default Parent;
