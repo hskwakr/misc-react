e205cd89eb535b0aca1ddf80693de2f4038af439
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-21 16:22:12 +0900

    Create UserSearch.tsx
    22. Applying Types to Refs

---
+++ projects/udemy/react-and-typescript-build-a-portfolio-project/rts/src/refs/UserSearch.tsx
@@ -0,0 +1,55 @@
+import { useRef, useState } from 'react';
+
+const users = [
+  { name: 'Sarah', age: 18 },
+  { name: 'Alex', age: 20 },
+  { name: 'Michael', age: 22 },
+];
+
+const UserSearch = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [name, setName] = useState('');
+  const [foundUsers, setFoundUsers] = useState<typeof users>([]);
+
+  const onChangeHandler: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setName(e.target.value);
+  };
+
+  const onClickHandler = () => {
+    const found = users.filter((u) => name !== '' && u.name.includes(name));
+    setFoundUsers(found);
+  };
+
+  const searchResult =
+    foundUsers.length > 0 ? (
+      <ul>
+        {foundUsers.map((user) => (
+          <li key={user.name}>
+            {user.name} {user.age}
+          </li>
+        ))}
+      </ul>
+    ) : (
+      <p>User Not Found</p>
+    );
+
+  return (
+    <div>
+      <h3>User Search</h3>
+      <div>
+        <input
+          ref={inputRef}
+          type="text"
+          onChange={onChangeHandler}
+          value={name}
+        />
+        <button type="button" onClick={onClickHandler}>
+          Find User
+        </button>
+      </div>
+      {searchResult}
+    </div>
+  );
+};
+
+export default UserSearch;
