a4ce0f0f8145e15abe6fd61bb509bc117bee9d2e
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-22 07:37:59 +0900

    Add prettier and eslint

---
+++ projects/udemy/react-and-typescript-build-a-portfolio-project/redux-ts/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'airbnb',
+    'airbnb-typescript',
+    'prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
+};
