b051d89af2f73e932514bd08c5ec1dce2855dbb1
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-22 08:18:07 +0900

    Add eslint settings

--- projects/udemy/react-and-typescript-build-a-portfolio-project/redux-ts/.eslintrc.js
+++ projects/udemy/react-and-typescript-build-a-portfolio-project/redux-ts/.eslintrc.js
@@ -4,18 +4,45 @@ module.exports = {
     es2021: true,
   },
   extends: [
+    'eslint:recommended',
     'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'standard-with-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'airbnb',
+    'airbnb/hooks',
     'airbnb-typescript',
     'prettier',
   ],
   overrides: [],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
+    project: 'tsconfig.json',
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react'],
-  rules: {},
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/function-component-definition': [
+      'warn',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: [
+    '.eslintrc.js',
+    'src/react-app-env.d.ts',
+    'src/reportWebVitals.ts',
+  ],
+  root: true,
 };
