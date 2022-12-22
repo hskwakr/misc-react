e8295c6e08bbb0e393d4f84f0f9c8c43e139e7fd
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-22 11:22:24 +0900

    Update repositoriesReducer.ts
    32. Applying Action Interfaces

--- projects/udemy/react-and-typescript-build-a-portfolio-project/redux-ts/src/state/reducers/repositoriesReducer.ts
+++ projects/udemy/react-and-typescript-build-a-portfolio-project/redux-ts/src/state/reducers/repositoriesReducer.ts
@@ -1,24 +1,44 @@
 interface RepositoriesState {
   loading: boolean;
-  error: string | null;
   data: string[];
+  error: string | null;
 }
 
-interface Action {
-  type: string;
-  payload?: any;
+const ActionType = {
+  SEARCH: 'search_repositories',
+  SEARCH_SUCCESS: 'search_repositories_success',
+  SEARCH_ERROR: 'search_repositories_error',
+} as const;
+
+interface SearchRepositoriesAction {
+  type: typeof ActionType.SEARCH;
 }
+interface SearchRepositoriesSuccessAction {
+  type: typeof ActionType.SEARCH_SUCCESS;
+  payload: RepositoriesState['data'];
+}
+interface SearchRepositoriesErrorAction {
+  type: typeof ActionType.SEARCH_ERROR;
+  payload: string;
+}
+
+type Action =
+  | SearchRepositoriesAction
+  | SearchRepositoriesSuccessAction
+  | SearchRepositoriesErrorAction;
 
 const reducer = (
   state: RepositoriesState,
   action: Action
 ): RepositoriesState => {
   switch (action.type) {
-    case 'search_repositories':
+    case ActionType.SEARCH:
       return { loading: true, error: null, data: [] };
-    case 'search_repositories_success':
+
+    case ActionType.SEARCH_SUCCESS:
       return { loading: false, error: null, data: action.payload };
-    case 'search_repositories_error':
+
+    case ActionType.SEARCH_ERROR:
       return { loading: false, error: action.payload, data: [] };
 
     default:
