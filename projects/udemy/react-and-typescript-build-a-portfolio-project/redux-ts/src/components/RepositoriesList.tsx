50e3b31fa8c3688d9f05920afed313c9672fdfe4
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-24 07:49:49 +0900

    Update example of RTBP
    49. Creating a Typed Selector

--- projects/udemy/react-and-typescript-build-a-portfolio-project/redux-ts/src/components/RepositoriesList.tsx
+++ projects/udemy/react-and-typescript-build-a-portfolio-project/redux-ts/src/components/RepositoriesList.tsx
@@ -1,14 +1,19 @@
 import { useState } from 'react';
 import { useActions } from '../hooks/useActions';
+import { useTypedSelector } from '../hooks/useTypedSelector';
 
 const RepositoriesList = () => {
   const [term, setTerm] = useState('');
   const { searchRepositories } = useActions();
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  );
 
   const onSubmit: React.FormEventHandler = (e) => {
     e.preventDefault();
 
     searchRepositories(term);
+    console.log(data, error, loading);
   };
 
   const onChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
