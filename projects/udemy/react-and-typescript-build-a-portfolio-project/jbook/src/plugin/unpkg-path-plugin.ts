40f9ec2a313c78fa0cae04616afdec7917a9c8cf
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-27 08:30:02 +0900

    Update unpkg-path-plugin.ts
    75. Deep Dive on Building

--- projects/udemy/react-and-typescript-build-a-portfolio-project/jbook/src/plugin/unpkg-path-plugin.ts
+++ projects/udemy/react-and-typescript-build-a-portfolio-project/jbook/src/plugin/unpkg-path-plugin.ts
@@ -3,12 +3,12 @@ import * as esbuild from 'esbuild-wasm';
 export const unpkgPathPlugin = () => ({
   name: 'unpkg-path-plugin',
   setup(build: esbuild.PluginBuild) {
-    build.onResolve({ filter: /.*/ }, async (args: any) => ({
+    build.onResolve({ filter: /.*/ }, async (args: esbuild.OnResolveArgs) => ({
       path: args.path,
       namespace: 'a',
     }));
 
-    build.onLoad({ filter: /.*/ }, async (args: any) => {
+    build.onLoad({ filter: /.*/ }, async (args: esbuild.OnLoadArgs) => {
       if (args.path === 'index.js') {
         return {
           loader: 'jsx',
