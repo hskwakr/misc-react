b4b37c8317f544f6f9c7794ab43fb74ca1061ec8
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-12-30 22:58:19 +0900

    Update fetch-plugin.ts
    100. Separate Load Filters

--- projects/udemy/react-and-typescript-build-a-portfolio-project/jbook/src/plugin/fetch-plugin.ts
+++ projects/udemy/react-and-typescript-build-a-portfolio-project/jbook/src/plugin/fetch-plugin.ts
@@ -9,20 +9,18 @@ const fileCache = localforage.createInstance({
 export const fetchPlugin = (inputCode: string) => ({
   name: 'fetch-plugin',
   setup(build: esbuild.PluginBuild) {
-    build.onLoad({ filter: /.*/ }, async (args: esbuild.OnLoadArgs) => {
-      if (args.path === 'index.js') {
-        return {
-          loader: 'jsx',
-          contents: inputCode,
-        };
+    build.onLoad({ filter: /(^index\.js$)/ }, () => ({
+      loader: 'jsx',
+      contents: inputCode,
+    }));
+    
+    build.onLoad({ filter: /\.css$/ }, async (args: esbuild.OnLoadArgs) => {
+      const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+        args.path
+      );
+      if (cachedResult != null) {
+        return cachedResult;
       }
-
-      // const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-      //   args.path
-      // );
-      // if (cachedResult != null) {
-      //   return cachedResult;
-      // }
 
       const res = await axios.get<string>(args.path);
       const { data } = res;
@@ -30,25 +28,45 @@ export const fetchPlugin = (inputCode: string) => ({
         throw new Error('Unexpected responce.request');
       }
 
-      const fileType = args.path.match(/\.css$/) != null ? 'css' : 'jsx';
-
       const escaped = data
         .replace(/\n/g, '')
         .replace(/"/g, '\\"')
         .replace(/'/g, "\\'");
 
-      const contents =
-        fileType === 'css'
-          ? `
-          conststyle = document.createElement('style');
-          style.innerText = '${escaped}';
-          document.head.appendChild(style);
-          `
-          : data;
+      const contents = `
+        conststyle = document.createElement('style');
+        style.innerText = '${escaped}';
+        document.head.appendChild(style);
+      `;
 
       const result: esbuild.OnLoadResult = {
         loader: 'jsx',
         contents,
+        resolveDir: new URL('./', res.request.responseURL).pathname,
+      };
+
+      await fileCache.setItem(args.path, result);
+
+      return result;
+    });
+
+    build.onLoad({ filter: /.*/ }, async (args: esbuild.OnLoadArgs) => {
+      const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+        args.path
+      );
+      if (cachedResult != null) {
+        return cachedResult;
+      }
+
+      const res = await axios.get<string>(args.path);
+      const { data } = res;
+      if (res.request === null || !(res.request instanceof XMLHttpRequest)) {
+        throw new Error('Unexpected responce.request');
+      }
+
+      const result: esbuild.OnLoadResult = {
+        loader: 'jsx',
+        contents: data,
         resolveDir: new URL('./', res.request.responseURL).pathname,
       };
 
