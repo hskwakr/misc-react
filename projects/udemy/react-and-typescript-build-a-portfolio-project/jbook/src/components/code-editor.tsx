00439487031ffc74861798b444c3e86a79d3bf18
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-01-05 15:32:13 +0900

    Update code-editor.tsx
    136. Adding Prettier to Our Editor

--- projects/udemy/react-and-typescript-build-a-portfolio-project/jbook/src/components/code-editor.tsx
+++ projects/udemy/react-and-typescript-build-a-portfolio-project/jbook/src/components/code-editor.tsx
@@ -1,31 +1,61 @@
 import Editor from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
+import { useState } from 'react';
 
 interface CodeEditorProps {
   initialValue: string;
   onChange: (value: string) => void;
 }
 
-const CodeEditor = ({ initialValue, onChange }: CodeEditorProps) => (
-  <Editor
-    height="500px"
-    theme="vs-dark"
-    defaultLanguage="javascript"
-    defaultValue={initialValue}
-    onChange={(value) => {
-      onChange(value ?? '');
-    }}
-    options={{
-      wordWrap: 'on',
-      minimap: { enabled: false },
-      showUnused: false,
-      folding: false,
-      lineNumbersMinChars: 3,
-      tabSize: 2,
-      fontSize: 16,
-      scrollBeyondLastLine: false,
-      automaticLayout: true,
-    }}
-  />
-);
+const format = (code: string) =>
+  prettier.format(code, {
+    parser: 'babel',
+    plugins: [parser],
+  });
+
+const CodeEditor = ({ initialValue, onChange }: CodeEditorProps) => {
+  const [code, setCode] = useState(format(initialValue));
+
+  const onFormatClick = () => {
+    const newCode = format(code);
+
+    setCode(newCode);
+    onChange(newCode);
+  };
+
+  return (
+    <>
+      <button type="button" onClick={onFormatClick}>
+        Format
+      </button>
+
+      <Editor
+        height="500px"
+        theme="vs-dark"
+        defaultLanguage="javascript"
+        value={code}
+        onChange={(value) => {
+          if (value == null) {
+            return;
+          }
+          setCode(value);
+          onChange(value);
+        }}
+        options={{
+          wordWrap: 'on',
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          tabSize: 2,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+      />
+    </>
+  );
+};
 
 export default CodeEditor;
