b0d0a7daddd76cfcb2cfe15fb11271dd2c256885
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-01-07 10:32:23 +0900

    Extract to CodeCell component
    146. Multiple Editors and Preview Windows

--- projects/udemy/react-and-typescript-build-a-portfolio-project/jbook/src/App.tsx
+++ projects/udemy/react-and-typescript-build-a-portfolio-project/jbook/src/App.tsx
@@ -1,48 +1,11 @@
-import { useState } from 'react';
-import CodeEditor from './components/code-editor';
-import Preview from './components/preview';
-import bundle from './bundler';
+import CodeCell from './components/code-cell';
 
-const initialValue = `
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const App = () => {
-  return <h1>Hi there!</h1>;
-};
-
-ReactDOM.render(<App />, document.querySelector('#root'));
-`;
-
-const App = () => {
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
-
-  const onClick = async () => {
-    const output = await bundle(input)
-    setCode(output);
-  };
-
-  return (
-    <div>
-      <CodeEditor
-        initialValue={initialValue}
-        onChange={(value) => setInput(value)}
-      />
-      <textarea value={input} onChange={(e) => setInput(e.target.value)} />
-      <div>
-        <button
-          type="button"
-          onClick={() => {
-            void onClick();
-          }}
-        >
-          Submit
-        </button>
-      </div>
-      <Preview code={code} />
-    </div>
-  );
-};
+const App = () => (
+  <div>
+    <CodeCell />
+    <CodeCell />
+    <CodeCell />
+  </div>
+);
 
 export default App;
---
+++ projects/udemy/react-and-typescript-build-a-portfolio-project/jbook/src/components/code-cell.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import CodeEditor from './code-editor';
+import Preview from './preview';
+import bundle from '../bundler';
+
+const initialValue = `
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <h1>Hi there!</h1>;
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+`;
+
+const CodeCell = () => {
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const onClick = async () => {
+    const output = await bundle(input);
+    setCode(output);
+  };
+
+  return (
+    <div>
+      <CodeEditor
+        initialValue={initialValue}
+        onChange={(value) => setInput(value)}
+      />
+      <div>
+        <button
+          type="button"
+          onClick={() => {
+            void onClick();
+          }}
+        >
+          Submit
+        </button>
+      </div>
+      <Preview code={code} />
+    </div>
+  );
+};
+
+export default CodeCell;
