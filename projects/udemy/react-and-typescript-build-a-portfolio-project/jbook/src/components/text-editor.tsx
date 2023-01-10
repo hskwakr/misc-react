bfecb2e754ed70e9b31886829f45b4b38fb99d96
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-01-10 13:29:34 +0900

    Update text-editor.tsx
    182. Toggling the Markdown Editor Mode

--- projects/udemy/react-and-typescript-build-a-portfolio-project/jbook/src/components/text-editor.tsx
+++ projects/udemy/react-and-typescript-build-a-portfolio-project/jbook/src/components/text-editor.tsx
@@ -1,16 +1,30 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import MDEditor from '@uiw/react-md-editor';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const TextEditor = () => {
+  const [editing, setEditing] = useState(false);
   const [value, setValue] = useState('');
-  return (
+
+  useEffect(() => {
+    const listener = () => {
+      setEditing(false);
+    };
+    document.addEventListener('click', listener, { capture: true });
+    
+    return () => {
+      document.removeEventListener('click', listener, { capture: true });
+    }
+  }, []);
+
+  return editing ? (
     <div>
-      <MDEditor
-        value={value}
-        onChange={(v) => {
-          setValue(v ?? '');
-        }}
-      />
+      <MDEditor value={value} onChange={(v) => setValue(v ?? '')} />
+    </div>
+  ) : (
+    <div onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source="# Header" />
     </div>
   );
 };
