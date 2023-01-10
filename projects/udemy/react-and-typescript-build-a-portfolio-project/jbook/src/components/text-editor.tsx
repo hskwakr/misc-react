277b8dcb1331c2651c91f3b2129623300a7e72c7
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-01-10 13:41:15 +0900

    Update text-editor.tsx
    183. Conditional Toggling

--- projects/udemy/react-and-typescript-build-a-portfolio-project/jbook/src/components/text-editor.tsx
+++ projects/udemy/react-and-typescript-build-a-portfolio-project/jbook/src/components/text-editor.tsx
@@ -1,25 +1,34 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import MDEditor from '@uiw/react-md-editor';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const TextEditor = () => {
   const [editing, setEditing] = useState(false);
   const [value, setValue] = useState('');
+  const divRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    const listener = () => {
+    const listener = (event: MouseEvent) => {
+      if (
+        divRef.current != null &&
+        event.target != null &&
+        divRef.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+
       setEditing(false);
     };
     document.addEventListener('click', listener, { capture: true });
-    
+
     return () => {
       document.removeEventListener('click', listener, { capture: true });
-    }
+    };
   }, []);
 
   return editing ? (
-    <div>
+    <div ref={divRef}>
       <MDEditor value={value} onChange={(v) => setValue(v ?? '')} />
     </div>
   ) : (
