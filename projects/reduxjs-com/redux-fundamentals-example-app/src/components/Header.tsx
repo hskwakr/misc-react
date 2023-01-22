14e9d50803e2ca04fcab25c27cec0327be137ed7
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-01-22 15:49:54 +0900

    Update Header.tsx

--- projects/reduxjs-com/redux-fundamentals-example-app/src/components/Header.tsx
+++ projects/reduxjs-com/redux-fundamentals-example-app/src/components/Header.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
+import { useAppDispatch } from '../redux/hooks';
 
 const Header = () => {
   const [text, setText] = useState('');
+  const dispatch = useAppDispatch();
 
   return (
     <header className="header">
@@ -9,7 +11,23 @@ const Header = () => {
         className="new-todo"
         placeholder="What needs to be done?"
         value={text}
+        autoFocus={true}
         onChange={(e) => setText(e.target.value)}
+        onKeyDown={(e) => {
+          const trimmed = e.currentTarget.value.trim();
+
+          // If the user pressed the Enter key:
+          if (e.key === 'Enter' && trimmed) {
+            // Dispatch the "todo added" action with this text
+            dispatch({
+              type: 'todos/todoAdded',
+              payload: trimmed,
+            });
+
+            // And clear out the text input
+            setText('');
+          }
+        }}
       />
     </header>
   );
