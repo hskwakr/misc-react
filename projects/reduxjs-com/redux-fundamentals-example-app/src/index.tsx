6991083eff2760787d285b01c44cfb9a88c35fd9
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-01-22 19:22:08 +0900

    Update index.tsx
    Passing the Store with Provider
    https://redux.js.org/tutorials/fundamentals/part-5-ui-react

--- projects/reduxjs-com/redux-fundamentals-example-app/src/index.tsx
+++ projects/reduxjs-com/redux-fundamentals-example-app/src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Render a `<Provider>` around the entire `<App>`, */}
+    {/* and pass the Redux store to as a prop */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
