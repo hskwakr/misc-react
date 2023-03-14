03903bebf30a37b050e05735411f1dfb2dbbe880
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-03-14 11:14:00 +0900

    Format codes by prettier

--- projects/florinpop17/app-ideas/bin2dec/src/App.tsx
+++ projects/florinpop17/app-ideas/bin2dec/src/App.tsx
@@ -1,5 +1,7 @@
 import './App.css';
 
-const App = () => <h1 className="text-3xl font-bold underline">Hello world!</h1>
+const App = () => (
+  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+);
 
 export default App;
