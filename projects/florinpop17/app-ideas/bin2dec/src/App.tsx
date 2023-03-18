b5e189a311920ca89943fe080013a0aa899ceeab
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-03-19 07:26:49 +0900

    Add Binaries Component

--- projects/florinpop17/app-ideas/bin2dec/src/App.tsx
+++ projects/florinpop17/app-ideas/bin2dec/src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css';
+import Binaries from './components/Binaries';
 
 const App = () => (
-  <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  <>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <Binaries />
+  </>
 );
 
 export default App;
