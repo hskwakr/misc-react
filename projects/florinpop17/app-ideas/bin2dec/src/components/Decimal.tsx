08ccb4f4df0d4adee7ab51534905ef5fee224df6
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-03-20 06:20:07 +0900

    Update Decimal.tsx

--- projects/florinpop17/app-ideas/bin2dec/src/components/Decimal.tsx
+++ projects/florinpop17/app-ideas/bin2dec/src/components/Decimal.tsx
@@ -1,7 +1,11 @@
 const Decimal = () => {
   return (
-    <div className="m-auto max-w-xs bg-amber-500 text-amber-100">
-      <div className="p-4 text-center  font-mono text-8xl">{255}</div>
+    <div className="m-auto max-w-xs">
+      <div className="m-1.5 bg-amber-500">
+        <div className="p-4 text-center font-mono text-8xl text-amber-100">
+          {255}
+        </div>
+      </div>
     </div>
   );
 };
