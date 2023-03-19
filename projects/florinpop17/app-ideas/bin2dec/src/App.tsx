3b2a4fdb8c021d6f4c7c08e6edeeb3db5273e623
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-03-20 06:20:47 +0900

    Update App.tsx

--- projects/florinpop17/app-ideas/bin2dec/src/App.tsx
+++ projects/florinpop17/app-ideas/bin2dec/src/App.tsx
@@ -3,11 +3,15 @@ import Binary from './components/Binariy';
 import Decimal from './components/Decimal';
 
 const App = () => (
-  <>
-    <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    <Binary />
-    <Decimal />
-  </>
+  <div className="h-screen">
+    <div className="flex h-16 bg-gray-500">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+    <div className="h-fit bg-slate-400 pt-16">
+      <Binary />
+      <Decimal />
+    </div>
+  </div>
 );
 
 export default App;
