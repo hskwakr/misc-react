eb99787fc0a94a56cdaedbc1622e00e34708f512
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-03-20 04:58:48 +0900

    Add Decimal component

--- projects/florinpop17/app-ideas/bin2dec/src/App.tsx
+++ projects/florinpop17/app-ideas/bin2dec/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Binary from './components/Binariy';
+import Decimal from './components/Decimal';
 
 const App = () => (
   <>
     <h1 className="text-3xl font-bold underline">Hello world!</h1>
     <Binary />
+    <Decimal />
   </>
 );
 
---
+++ projects/florinpop17/app-ideas/bin2dec/src/components/Decimal.tsx
@@ -0,0 +1,9 @@
+const Decimal = () => {
+  return (
+    <div className="m-auto max-w-xs bg-amber-500 text-amber-100">
+      <div className="p-4 text-center  font-mono text-8xl">{255}</div>
+    </div>
+  );
+};
+
+export default Decimal;
