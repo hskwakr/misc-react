594554d51f26b8b86b11c447e1539ee292b9f927
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-04-07 16:13:51 +0900

    Update Binariy.tsx

--- projects/florinpop17/app-ideas/bin2dec/src/components/Binariy.tsx
+++ projects/florinpop17/app-ideas/bin2dec/src/components/Binariy.tsx
@@ -4,31 +4,33 @@ type Binary = 0 | 1;
 const init8BitBinary: Binary[] = [0, 0, 0, 0, 0, 0, 0, 0];
 
 const Binary = () => {
-  const items = init8BitBinary.map(bin => (
-    <div className="mx-auto my-2 bg-amber-300">
-      <BinaryCounter value={bin} />
-    </div>
-  ));
+  const [binaries, setBinaries] = useState(init8BitBinary);
+
+  const items = binaries.map((bin, idx) => {
+    const updateBinary = () => {
+      setBinaries(prev => {
+        const newState = prev.map((v, i) => {
+          return i === idx ? (v ? 0 : 1) : v;
+        });
+
+        return newState;
+      });
+    };
+
+    return (
+      <div className="mx-auto my-2 bg-amber-300">
+        <div
+          onClick={updateBinary}
+          aria-hidden="true"
+          className="rounded-lg bg-amber-100 px-2"
+        >
+          <div className="font-mono text-8xl">{bin}</div>
+        </div>
+      </div>
+    );
+  });
 
   return <div className="mx-auto grid max-w-xs grid-cols-4">{items}</div>;
 };
 
 export default Binary;
-
-const BinaryCounter = ({ value }: { value: Binary }) => {
-  const [binary, setBinary] = useState(value);
-
-  const updateBinary = () => {
-    setBinary(binary ? 0 : 1);
-  };
-
-  return (
-    <div
-      onClick={updateBinary}
-      aria-hidden="true"
-      className="rounded-lg bg-amber-100 px-2"
-    >
-      <div className="font-mono text-8xl">{binary}</div>
-    </div>
-  );
-};
