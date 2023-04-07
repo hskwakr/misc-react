1a31f03d6df14d9fd9d95040d2b0f2dd464ac459
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-04-07 17:23:08 +0900

    Extract to useBin2Dec

--- projects/florinpop17/app-ideas/bin2dec/src/App.tsx
+++ projects/florinpop17/app-ideas/bin2dec/src/App.tsx
@@ -1,29 +1,12 @@
-import { useState } from 'react';
 import Binary from './components/Binariy';
 import Decimal from './components/Decimal';
 import Navbar from './components/Navbar';
-import { Bin2Dec } from './lib/Bin2Dec';
-import { Bin } from './lib/Bin';
+import { useBin2Dec } from './hooks/useBin2Dec';
 
 import './App.css';
 
-const init8BitBinary: Bin[] = [0, 0, 0, 0, 0, 0, 0, 0];
-
 const App = () => {
-  const [binaries, setBinaries] = useState(init8BitBinary);
-
-  const handleBinaries = (idx: number) => {
-    setBinaries(prev => {
-      const newState = prev.map((v, i) => {
-        return i === idx ? (v ? 0 : 1) : v;
-      });
-
-      return newState;
-    });
-  };
-
-  const binStr = binaries.map(String).join('');
-  const decimal = Number(Bin2Dec(binStr));
+  const [binary, decimal, updateBin] = useBin2Dec();
 
   return (
     <div className="h-screen">
@@ -32,7 +15,7 @@ const App = () => {
       <div className="h-max pt-12">
         <div className="my-auto h-[450px]">
           <div className="h-2/3">
-            <Binary value={binaries} update={handleBinaries} />
+            <Binary value={binary} update={updateBin} />
           </div>
 
           <div className="h-1/3">
