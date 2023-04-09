55354cf391c97b7ab743e1126c97a91779719c8a
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-04-10 06:50:15 +0900

    Update Decimal.tsx

--- projects/florinpop17/app-ideas/bin2dec/src/components/Decimal.tsx
+++ projects/florinpop17/app-ideas/bin2dec/src/components/Decimal.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from 'react';
+
+const easeOutQuad = (t: number) => t * (2 - t);
+const frameDuration = 1000 / 60;
+const duration = 2000;
+
 const Decimal = ({ value }: { value: number }) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    let frame = 0;
+    const totalFrames = Math.round(duration / frameDuration);
+    const counter = setInterval(() => {
+      frame++;
+      const progress = easeOutQuad(frame / totalFrames);
+      setCount(value * progress);
+
+      if (frame === totalFrames) {
+        clearInterval(counter);
+      }
+    }, frameDuration);
+  }, [value]);
+
+  const view = Math.round(count);
+
   return (
     <div className="m-auto max-w-xs">
       <div className="m-1.5 bg-amber-500">
         <div className="p-4 text-center font-mono text-8xl text-amber-100">
-          {value}
+          {view}
         </div>
       </div>
     </div>
