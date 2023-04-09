73adbaf2ed4ce249fe330ddfaeb9b75f8aa73cca
hskwakr <33633391+hskwakr@users.noreply.github.com>
2023-04-10 07:22:05 +0900

    Update useCounter.ts

--- projects/florinpop17/app-ideas/bin2dec/src/hooks/useCounter.ts
+++ projects/florinpop17/app-ideas/bin2dec/src/hooks/useCounter.ts
@@ -3,13 +3,13 @@ import { useEffect, useState } from 'react';
 const easeOutQuad = (t: number) => t * (2 - t);
 const frameDuration = 1000 / 60;
 const duration = 1000;
+const totalFrames = Math.round(duration / frameDuration);
 
 export const useCounter = (value: number) => {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
     let frame = 0;
-    const totalFrames = Math.round(duration / frameDuration);
 
     const timer = setInterval(() => {
       frame++;
